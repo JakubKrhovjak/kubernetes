3f6c9a161cca6a78d8d782fd22ee669071e8264a
lucky <jakub.krhovjak@proronmail.com>
2022-10-28 16:10:50 +0200

    config maps

--- simple-rest/src/main/java/com/example/restapp/SimpleRest.java
+++ simple-rest/src/main/java/com/example/restapp/SimpleRest.java
@@ -1,13 +1,21 @@
 package com.example.restapp;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnCloudPlatform;
+import org.springframework.boot.cloud.CloudPlatform;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @SpringBootApplication
+@RequiredArgsConstructor
 public class SimpleRest {
 
     public static void main(String[] args) {
@@ -17,6 +25,19 @@ public class SimpleRest {
     @GetMapping("/test")
     public String test() {
         return "simple-rest app";
+    }
+
+    @Value("${message:nula}")
+    private String message;
+
+
+    private final Environment environment;
+
+    @ConditionalOnCloudPlatform(CloudPlatform.KUBERNETES)
+    @EventListener({ ApplicationReadyEvent.class})
+    public void begin() {
+        System.out.println(this.environment.getProperty("message"));
+        System.out.println(" @Value: " + message);
     }
 
 }
