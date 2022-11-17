943227142607cd35fb38bbcb03c5e3bbacf6a942
lucky <jakub.krhovjak@proronmail.com>
2022-11-17 15:26:05 +0100

    ????

--- web-flux/src/main/java/com/example/webflux/security/SecurityConfig.java
+++ web-flux/src/main/java/com/example/webflux/security/SecurityConfig.java
@@ -1,33 +1,21 @@
 package com.example.webflux.security;
 
-import com.fasterxml.jackson.core.filter.TokenFilter;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.DelegatingReactiveAuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.authentication.ReactiveAuthenticationManagerAdapter;
-import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
 import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+
+
+import java.util.List;
 
 /**
  * Created by Jakub Krhovják on 11/5/22.
@@ -42,22 +30,21 @@ public class SecurityConfig  {
 //    private String apiToken;
 
 
-    public ReactiveAuthenticationManagerAdapter customAuthManager(HttpSecurity http) throws Exception {
-        var authManager = http.getSharedObject(AuthenticationManagerBuilder.class)
-                .authenticationProvider(new BearerAuthenticationProvider(bearerToken))
-//                .authenticationProvider(new ApiTokenAuthenticationProvider(apiToken))
-                .build();
-
-        return new ReactiveAuthenticationManagerAdapter(authManager);
-    }
+//    @Bean
+//    public ReactiveAuthenticationManager customAuthManager(ServerHttpSecurity http) throws Exception {
+//        var authManager = new ProviderManager(List.of(new BearerAuthenticationProvider(bearerToken)));
+//        return new ReactiveAuthenticationManagerAdapter(authManager);
+//    }
 
 
     @Bean
-    @Order(2)
+    @Order(1)
     public SecurityWebFilterChain tokenFilterChain(ServerHttpSecurity http) throws Exception {
+        var authManager = new ProviderManager(List.of(new BearerAuthenticationProvider(bearerToken)));
+
         return http
                 .securityMatcher(ServerWebExchangeMatchers.pathMatchers("/webflux/token/**"))
-//                .addFilterAt(new TokenFilter(customAuthManager(http)), AuthenticationWebFilter.class)
+                .addFilterAt(new TokenFilter(new ReactiveAuthenticationManagerAdapter(authManager)), SecurityWebFiltersOrder.AUTHENTICATION)
                 .authorizeExchange().pathMatchers("/webflux/token/**").authenticated()
                 .and()
 
--- web-flux/src/main/java/com/example/webflux/security/TokenFilter.java
+++ web-flux/src/main/java/com/example/webflux/security/TokenFilter.java
@@ -5,8 +5,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
@@ -15,6 +17,7 @@ import org.springframework.web.server.WebFilterChain;
 import java.util.Collections;
 import java.util.List;
 
+import io.netty.util.internal.StringUtil;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
 
@@ -24,33 +27,32 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class TokenFilter implements WebFilter {
 
-    private static final String AUTHORIZATION = "authorization";
+    private static final String AUTHORIZATION = "Authorization";
     private static final String API_KEY = "api-key";
     private final ReactiveAuthenticationManager authenticationManager;
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-        try {
-            var authentication = authenticationManager.authenticate(authorization(exchange.getRequest()));
-//            if (authentication.is()) {
-//                SecurityContextHolder.getContext().setAuthentication(authentication);
-//                return chain.filter(exchange);
-//            }
-        } catch (Exception e) {
-            var response = exchange.getResponse();
-//            response.setHeader("WWW-Authenticate", "token-Security");
-//            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, HttpStatus.UNAUTHORIZED.getReasonPhrase());
-        }
 
-        return Mono.empty();
+     return authenticationManager.authenticate(authorization(exchange.getRequest()))
+                .doOnNext(authentication -> {
+                    ReactiveSecurityContextHolder.getContext()
+                            .subscriberContext((ReactiveSecurityContextHolder.withAuthentication(authentication)));
+                    chain.filter(exchange);
+                    return Mono.empty();
+                })
+             .onErrorMap(e -> {
+                 exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                 exchange.getResponse().getHeaders().set(HttpHeaders.WWW_AUTHENTICATE, "Bearer");
+                 return Mono.empty();
+             });
+
     }
 
-    private Authentication authorization(ServerHttpRequest request) {
-        HttpHeaders headers = request.getHeaders();
-        List<String> strings = headers.get(AUTHORIZATION);
-//        var headers = Collections.list(request.getHeaderNames());     String header = request.getHeader(AUTHORIZATION);
-        if (headers.containsValue(AUTHORIZATION)) {
-            return new BearerAuthentication(false, headers.get(AUTHORIZATION));
+    private static Authentication authorization(ServerHttpRequest request) {
+        var headers = request.getHeaders();
+        if (headers.containsKey(AUTHORIZATION)) {
+            return new BearerAuthentication(false, headers.get(AUTHORIZATION).stream().findFirst().orElse(StringUtil.EMPTY_STRING));
         }
         return  null;
 
