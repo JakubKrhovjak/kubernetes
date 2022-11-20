7472efae129bede970105caca226eeca4dbeb696
lucky <jakub.krhovjak@proronmail.com>
2022-11-20 10:44:52 +0100

    app2

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,20 @@
+import {useEffect} from "react";
+import axios from "axios";
+
 function App() {
-  return (
-    <div className="App">
-        <h1>React</h1>
-    </div>
-  );
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/simple-rest/test")
+            .then(res => console.log(res));
+
+    })
+
+    return (
+        <div className="App">
+            <h1>React</h1>
+        </div>
+    );
+
 }
 
 export default App;
